c453cf10f0aec91586a3c4f6ff88e36a1db5b112
Sans <144264225+SansGrabie@users.noreply.github.com>
2023-09-12 19:46:19 +0200

    Update main.php

--- main.php
+++ main.php
@@ -8,12 +8,29 @@
     <title>Strona Główna</title>
 </head>
 <body>
-<?php
-include ('header.html');
-?>
+    <div id="header-container">
+        <header id="header">
+            <nav>
+                <li onclick="openNav()" id="navigationOpen"><a>Nawigacja &#8595;</a></li>
+                <li onclick="closedNav()" id="navigationClosed"><a>Nawigacja &#8595;</a></li>
+            </nav>
+            <aside id="sidenav">
+                <li><a href="main.php">Główna Strona</a></li>
+                <li><a href="#">#</a></li>
+                <li><a href="#">#</a></li>
+                <li><a href="#">#</a></li>
+                <li><a href="#">#</a></li> 
+            </aside>
+            <div id="User-tools">
+                <a href="logowanie.php" class="right">Zaloguj</a>
+            </div>
+        </header>
+    </div>
+    <div id="color-border"></div>
     <div id="content">
         <main id="index"></main>
     </div>
 <!--<script src="navbar.js"></script>-->
+    <script src="header-close-open.js"></script>
 </body>
 </html>
