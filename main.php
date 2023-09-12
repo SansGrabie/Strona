218d76681d9a5d2a349b388b97bb9d4ca87008b8
Sans <144264225+SansGrabie@users.noreply.github.com>
2023-09-12 19:39:21 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Strona Główna</title>
+</head>
+<body>
+<?php
+include ('header.html');
+?>
+    <div id="content">
+        <main id="index"></main>
+    </div>
+<!--<script src="navbar.js"></script>-->
+</body>
+</html>
